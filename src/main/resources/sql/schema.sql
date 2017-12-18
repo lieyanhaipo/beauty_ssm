8380ebf6bec9d453bcb6fcab5771a367665834c0
dell <13028592007@163.com>
2017-12-18 12:23:32 +0800

    修改数据库脚本，增加用户，角色，权限表

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,55 +1,246 @@
+CREATE DATABASE  IF NOT EXISTS `beauty_ssm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `beauty_ssm`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: beauty_ssm
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
 
--- 需要 MySQL 5.6.5以上的版本
-CREATE DATABASE beauty_ssm;
-USE beauty_ssm;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 用户表
-CREATE TABLE _user(
-`user_id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-`user_name` VARCHAR(50) NOT NULL COMMENT '用户名',
-`user_phone` BIGINT NOT NULL COMMENT '手机号',
-`score` INT NOT NULL COMMENT '积分',
-`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-PRIMARY KEY (`user_id`),
-KEY `idx_user_phone`(`user_phone`)
-)ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='用户表';
+--
+-- Table structure for table `_goods`
+--
 
--- 商品表
-CREATE TABLE _goods(
-`goods_id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '商品ID',
-`title` VARCHAR(120) NOT NULL COMMENT '商品名称',
-`state` INT NOT NULL COMMENT '商品状态',
-`price` FLOAT NOT NULL COMMENT '商品价格',
-`number` INT NOT NULL COMMENT '商品数量',
-`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-`update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
-PRIMARY KEY (`goods_id`)
-)ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='商品表';
+DROP TABLE IF EXISTS `_goods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `_goods` (
+  `goods_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `title` varchar(120) NOT NULL COMMENT '商品名称',
+  `state` int(11) NOT NULL COMMENT '商品状态',
+  `price` float NOT NULL COMMENT '商品价格',
+  `number` int(11) NOT NULL COMMENT '商品数量',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`goods_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8 COMMENT='商品表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- 订单表
-CREATE TABLE _order(
-`order_id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '订单ID',
-`user_id` BIGINT NOT NULL  COMMENT '用户ID',
-`goods_id` BIGINT NOT NULL  COMMENT '商品ID',
-`title` VARCHAR(120) NOT NULL COMMENT '订单名称',
-`create_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-PRIMARY KEY (`order_id`),
-KEY `idx_user_id`(`user_id`),
-KEY `idx_goods_id`(`goods_id`)
-)ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT='订单表';
+--
+-- Dumping data for table `_goods`
+--
 
+LOCK TABLES `_goods` WRITE;
+/*!40000 ALTER TABLE `_goods` DISABLE KEYS */;
+INSERT INTO `_goods` VALUES (1000,'iphone7',1,3999,98,'2017-12-13 03:27:42','2017-12-13 03:27:42'),(1001,'ipad3',1,1999,2000,'2017-12-13 03:27:42','2017-12-13 03:27:42');
+/*!40000 ALTER TABLE `_goods` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- 插入初始数据
-INSERT INTO 
-	_user(user_name, user_phone, score)
-VALUES
-	('阿坚', 18768128888, 0),
-	('小明', 18968129999, 0);
+--
+-- Table structure for table `_order`
+--
 
+DROP TABLE IF EXISTS `_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `_order` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单ID',
+  `user_id` bigint(20) NOT NULL COMMENT '用户ID',
+  `goods_id` bigint(20) NOT NULL COMMENT '商品ID',
+  `title` varchar(120) NOT NULL COMMENT '订单名称',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`order_id`),
+  KEY `idx_user_id` (`user_id`),
+  KEY `idx_goods_id` (`goods_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8 COMMENT='订单表';
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO 
-	_goods(title, state, price,number)
-VALUES
-	('iphone7', 1, 3999, 100),
-	('ipad3', 1, 1999, 2000);
-	
+--
+-- Dumping data for table `_order`
+--
+
+LOCK TABLES `_order` WRITE;
+/*!40000 ALTER TABLE `_order` DISABLE KEYS */;
+INSERT INTO `_order` VALUES (1000,1000,1000,'抢购iphone','2017-12-13 03:28:56'),(1001,1000,1000,'抢购iphone','2017-12-13 03:42:16');
+/*!40000 ALTER TABLE `_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `_user`
+--
+
+DROP TABLE IF EXISTS `_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `user_name` varchar(50) NOT NULL COMMENT '用户名',
+  `user_phone` bigint(20) NOT NULL COMMENT '手机号',
+  `score` int(11) NOT NULL COMMENT '积分',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`user_id`),
+  KEY `idx_user_phone` (`user_phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=1002 DEFAULT CHARSET=utf8 COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `_user`
+--
+
+LOCK TABLES `_user` WRITE;
+/*!40000 ALTER TABLE `_user` DISABLE KEYS */;
+INSERT INTO `_user` VALUES (1000,'阿坚',18768128888,7100,'2017-12-13 03:27:42'),(1001,'小明',18968129999,7100,'2017-12-13 03:27:42');
+/*!40000 ALTER TABLE `_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_permission`
+--
+
+DROP TABLE IF EXISTS `sys_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_permission` (
+  `sys_permission_id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(64) DEFAULT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sys_permission_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='系统权限表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_permission`
+--
+
+LOCK TABLES `sys_permission` WRITE;
+/*!40000 ALTER TABLE `sys_permission` DISABLE KEYS */;
+INSERT INTO `sys_permission` VALUES (1,'login',NULL,'2017-12-15 09:54:34');
+/*!40000 ALTER TABLE `sys_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_role`
+--
+
+DROP TABLE IF EXISTS `sys_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_role` (
+  `sys_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(64) NOT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sys_role_id`),
+  UNIQUE KEY `sys_role_role_name_uindex` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='系统角色';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_role`
+--
+
+LOCK TABLES `sys_role` WRITE;
+/*!40000 ALTER TABLE `sys_role` DISABLE KEYS */;
+INSERT INTO `sys_role` VALUES (1,'admin',NULL,'2017-12-15 09:23:18');
+/*!40000 ALTER TABLE `sys_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_role_permission`
+--
+
+DROP TABLE IF EXISTS `sys_role_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_role_permission` (
+  `sys_role_id` int(11) DEFAULT NULL,
+  `sys_permission_id` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='角色权限关系表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_role_permission`
+--
+
+LOCK TABLES `sys_role_permission` WRITE;
+/*!40000 ALTER TABLE `sys_role_permission` DISABLE KEYS */;
+INSERT INTO `sys_role_permission` VALUES (1,1,1);
+/*!40000 ALTER TABLE `sys_role_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_user`
+--
+
+DROP TABLE IF EXISTS `sys_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_user` (
+  `sys_user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `created_time` datetime DEFAULT NULL,
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`sys_user_id`),
+  UNIQUE KEY `sys_user_user_name_uindex` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='系统用户';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user`
+--
+
+LOCK TABLES `sys_user` WRITE;
+/*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
+INSERT INTO `sys_user` VALUES (1,'admin','fc1709d0a95a6be30bc5926fdb7f22f4',NULL,'2017-12-15 06:57:11');
+/*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_user_role`
+--
+
+DROP TABLE IF EXISTS `sys_user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sys_user_id` int(11) DEFAULT NULL,
+  `sys_role_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='用户角色关系表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user_role`
+--
+
+LOCK TABLES `sys_user_role` WRITE;
+/*!40000 ALTER TABLE `sys_user_role` DISABLE KEYS */;
+INSERT INTO `sys_user_role` VALUES (1,1,1);
+/*!40000 ALTER TABLE `sys_user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-18 12:15:12
